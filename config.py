86ad61e692813d21acc15b750d4a4eb21ff0c6da
Vedant Sharma <Vedant.Sharma@qorvo.com>
2018-08-14 13:12:02 -0700

    First commit

---
+++ config.py
@@ -0,0 +1,3 @@
+DEBUG = True
+BCRYPT_LOG_ROUNDS = 12
+MAIL_FROM_EMAIL = "vedantsharma511@gmail.com"
---
+++ run.py
@@ -0,0 +1,24 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+app.config.from_object('config')
+app.config.from_pyfile('config.py')
+
+
+@app.route('/')
+def home():
+    return "Hello, World!"
+
+
+@app.route('/welcome')
+def welcome():
+    return render_template('welcome.html')
+
+
+@app.route('/login')
+def login():
+    return render_template('login.html')
+
+
+if __name__ == "__main__":
+    app.run()
