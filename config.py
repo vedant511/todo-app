b1f1913d0a377d13e93dd19b68e2d17c620625ab
Vedant Sharma <vedant.sharma@asu.edu>
2018-08-16 16:59:52 -0700

    Login functionality added, db error

--- config.py
+++ config.py
@@ -1,4 +1,3 @@
-DEBUG = True
+DEBUG = False
 BCRYPT_LOG_ROUNDS = 12
-MAIL_FROM_EMAIL = "vedantsharma511@gmail.com"
-DATABASE_URI = "mongodb+srv://vedant511:Rishi%25Honey%25518@web-app-cluster-1zkid.mongodb.net/test?retryWrites=true"
+DATABASE_URI = None
--- run.py
+++ run.py
@@ -1,8 +1,10 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
+from app.database import Database
+from models.user import User
 
-app = Flask(__name__)
-app.config.from_object('config')
-# app.config.from_pyfile('config.py')
+app = Flask(__name__, instance_relative_config=True)
+app.config.from_object('config')  # Uncomment this in Production
+app.config.from_pyfile('config.py')  # Uncomment this during development and staging
 
 
 @app.route('/')
@@ -15,10 +17,33 @@ def welcome():
     return render_template('welcome.html')
 
 
+@app.before_first_request
+def initialize_database():
+    Database.initialize()
+
+
 @app.route('/login')
 def login():
     return render_template('login.html')
 
 
+@app.route('/auth/login', methods=['POST'])
+def login_user():
+    identifier = request.form['email']
+    password = request.form['password']
+
+    if User.login(identifier, password):
+        if User.get_by_mail(identifier) is not None:
+            user = User.get_by_mail(identifier)
+        else:
+            user = User.get_by_unm(identifier)
+
+        return render_template('profile.html', name=user['name'])
+
+    else:
+        error = "Invalid credentials, please try again or register"
+        return render_template('login.html', error=error)
+
+
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run()
